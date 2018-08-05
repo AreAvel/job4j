9631bf4e06d09e4077902a015b1ff23b1b1b5918
Nikolay Tkachenko <Tkach1702@mail.ru>
2018-08-05 12:24:21 +0300

    JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j.calculate;
+
+/**
+*Class Calculate
+*@autor Nikolay Tkachenko (mailto:Tkach1702@mail.ru)
+*since 05.08.2018
+*/
 public class Calculate{
+
+     /**
+     * main. output to the console inscriptions.
+     * @param args - args.
+     */
 	public static void main(String[] agrs){
 		System.out.println("Hello World");
 		}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+*Package fo calculate task
+*
+*@autor Nikolay Tkachenko (mailto:Tkach1702@mail.ru)
+*@version $Id$
+*@since 05.08.2018
+*/
