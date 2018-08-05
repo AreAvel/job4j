94f5f773f92203199772d2bf24dfc97f1a80290c
Nikolay Tkachenko <Tkach1702@mail.ru>
2018-08-05 14:12:22 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,6 +3,7 @@ package ru.job4j.calculate;
 /**
 *Class Calculate
 *@author Nikolay Tkachenko (mailto:Tkach1702@mail.ru)
+*@version $Id$
 *since 05.08.2018
 */
 public class Calculate{
@@ -13,5 +14,13 @@ public class Calculate{
      */
 	public static void main(String[] agrs){
 		System.out.println("Hello World");
+		}
+     /**
+     * Method echo
+     * @param name Your name.
+     * @return Echo plus your name.		
+     */
+	public String echo(String name){
+		return "Echo, echo, echo: " + name;
 		}
 	}
