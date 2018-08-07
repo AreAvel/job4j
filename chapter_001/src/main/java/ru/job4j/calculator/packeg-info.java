dfb44c4cf9cd4b7f89780f57d1110754ba08fe78
Nikolay Tkachenko <Tkach1702@mail.ru>
2018-08-07 11:12:31 +0300

    package-info for Calculator and CalculatorTest

---
+++ chapter_001/src/main/java/ru/job4j/calculator/packeg-info.java
@@ -0,0 +1,7 @@
+/**
+ *Package fo Calculator
+ *
+ *@author Nikolay Tkachenko (mailto:Tkach1702@mail.ru)
+ *@version $Id$
+ *@since 1.0
+ */
---
+++ chapter_001/src/test/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *Package fo CalculatorTest
+ *
+ *@author Nikolay Tkachenko (mailto:Tkach1702@mail.ru)
+ *@version $Id$
+ *@since 1.0
+ */
